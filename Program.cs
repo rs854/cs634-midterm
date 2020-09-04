a9958986a3b53decae6b413fd8f95d77ae27ab4b
Robert Smith <rsmith8917@gmail.com>
2020-09-03 20:11:56 -0400

    new dotnet project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs634_midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
